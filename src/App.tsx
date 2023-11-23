557e199605081a8942b8b2208ed6517d4335f2db
Progress Aienobe <paienobe@gmail.com>
2023-11-23 16:56:17 +0100

    more-style-updates

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,11 @@ function App() {
   return (
     <>
       <Header />
-      <MysticSdk token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZGQyMWQzMGUyMjkxNDdiNjY1ZGM3YSIsImlhdCI6MTY5MjIxMzcxNX0.2RZ8He93TUUMeW0t3aqpt5KxNlo1r1fjMNtaBYyP9rI" />
+
+      <div className="sdk_wrapper">
+        <MysticSdk token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZGQyMWQzMGUyMjkxNDdiNjY1ZGM3YSIsImlhdCI6MTY5MjIxMzcxNX0.2RZ8He93TUUMeW0t3aqpt5KxNlo1r1fjMNtaBYyP9rI" />
+      </div>
+
       <Footer />
     </>
   );
